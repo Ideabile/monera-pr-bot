d1f3b1579f1a3704120eeb4649af4193afdadead
Mauro <mauro@insided.nl>
2015-12-13 21:47:19 +0100

    Make sure can be embended

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ import Server from './modules/Server.js';
 let options = process.argv.slice(2);
 
 export default (function(){
-  console.log(options);
-  if(options[0] === 'server') return new Server('localhost', 8081);
-  return new PullRequest(...options).create(()=>{console.log('PR created!')});
+  let dirName = __dirname.split('/');
+  dirName = dirName[dirName.length-1];
+  if(options[0] === 'server') eturn new Server('localhost', 8081);
+
+  for(let index in process.argv){
+    if((process.argv[index].indexOf(dirName) > -1)){
+      return new PullRequest(...options).create(()=>{console.log('PR created!')});
+    }
+  }
+  
+  return PullRequest;
 })();
