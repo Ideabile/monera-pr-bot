54543bf43aaded10e9c8f5e7dbb5f5d120304157
Mauro <mauro@insided.nl>
2015-12-13 21:48:14 +0100

    Typo...

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ let options = process.argv.slice(2);
 export default (function(){
   let dirName = __dirname.split('/');
   dirName = dirName[dirName.length-1];
-  if(options[0] === 'server') eturn new Server('localhost', 8081);
+  if(options[0] === 'server') return new Server('localhost', 8081);
 
   for(let index in process.argv){
     if((process.argv[index].indexOf(dirName) > -1)){
       return new PullRequest(...options).create(()=>{console.log('PR created!')});
     }
   }
-  
+
   return PullRequest;
 })();
